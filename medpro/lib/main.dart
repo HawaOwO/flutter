f2de532d536f690308e36c8dc1b4c578867fcc95
HawaOwO <dayinialhawani@student.usm.my>
2023-06-02 16:10:15 +0800

    login page

--- medpro/lib/main.dart
+++ medpro/lib/main.dart
@@ -1,58 +1,155 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MyCustomWidget extends StatefulWidget {
-  const MyCustomWidget({required Key key}) : super(key: key);
-
-  @override
-  State<MyCustomWidget> createState() => _MyCustomWidgetState();
+void main() {
+  runApp( MaterialApp(
+    theme: ThemeData(primaryColor: Colors.purple),
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+  ));
 }
 
-class _MyCustomWidgetState extends State<MyCustomWidget> {
-
-  var getResult = 'QR Code Result';
-
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('QR Scanner'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                scanQRCode();
-              },
-              child: Text('Scan QR'),
-            ),
-            SizedBox(height: 20.0,),
-            Text(getResult),
-          ],
-        )
-      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [ 
+                 Color(0xFF0288D1),
+                 Color(0xFF8A2387),
+                 Color(0xFF66BB6A),
+                 Color(0xFF80DEEA),
+              ]
+            )
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.
+            center,
+            children: [
+              SizedBox(height: 80,),
+              Image.asset('image/logo.png',
+              height:150),
+              SizedBox(height: 15,),
+              SizedBox(height: 30,),
+              Container(
+                height:480,
+                width: 325,
+                decoration: BoxDecoration(
+                  color:Colors.white,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 30,),
+                    Text('Hello',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    Text('Please log in to your account',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.grey
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
+                      width: 250,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email Address',
+                          suffixIcon: Icon(FontAwesomeIcons.envelope,
+                          size: 17,),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 250,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          suffixIcon: Icon(FontAwesomeIcons.eyeSlash,
+                          size: 17,),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 40, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text('Forget Password',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 170, 9, 3)
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  GestureDetector(
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 250,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [
+                            Color(0xFF0288D1),
+                            Color(0xFF8A2387),
+                            Color(0xFF66BB6A),
+                            Color(0xFF80DEEA),
+                          ]
+                        )
+                      ),
+                      child: Padding(
+                        padding:EdgeInsets.all(12.0),
+                        child: Text('Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                        ),
+                        ),
+                        ),
+                      ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text('or Login using Social Media',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),  
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(FontAwesomeIcons.solidEnvelope, color: Color.fromARGB(255, 53, 4, 4),),
+                    ],
+                  ),
+            
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
-
-  void scanQRCode() async {
-    try{
-      final qrCode = await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.QR);
-
-      if (!mounted) return;
-
-      setState(() {
-        getResult = qrCode;
-      });
-      print("QRCode_Result:--");
-      print(qrCode);
-    } on PlatformException {
-      getResult = 'Failed to scan QR Code.';
-    }
-
-  }
-
 }
